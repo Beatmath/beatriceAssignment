ec5d6af0749a260c923ed0f7ee88e0d709194cf6
BeatMath <mathiassen@telia.com>
2021-04-09 20:52:31 +0200

    The finally version

--- src/test/java/stepDefinitions/StepDefinition.java
+++ src/test/java/stepDefinitions/StepDefinition.java
@@ -10,8 +10,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.edge.EdgeDriver;
-//import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -23,175 +21,106 @@ import io.cucumber.java.en.When;
 /*Mailchimp*/
 
 public class StepDefinition {
-	
+
 	private WebDriver driver;
 	private LocalDateTime NowRightDate;
 	private DateTimeFormatter formDate;
 	private WebElement element;
-	
-	
-	
-		@Before
-			public void setUp() {
-			System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Selenium\\chromedriver.exe");
 
-		    driver = new ChromeDriver();
-		    
-		    driver.manage().window().maximize();   
-		    driver.get("https://login.mailchimp.com/signup//");
-		    
-		    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	@Before
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Selenium\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+		driver.get("https://login.mailchimp.com/signup//");
+	}
+
+	@Given("I add my {string} to create an Mialchimp account")
+	public void addEmail(String email) {
+		
+			sendKeysById("email", email);
 		}
-		
-		
-
-			/*@Given("I like to use this {string} browser")
-			public void i_like_to_use_this(String browser) {
-				
-				DriveCreator creator = new DriveCreator();
-			    
-			    driver = creator.createBrowser(browser);
-			}*/
-
-
-
 	
-		/*Creating random e-mail address*/
-		@Given("I add my {string} to create an Mialchimp account")
-		public void i_add_my_want_to_create_an_mialchimp_account(String email) {
-						
-			if (email.contains("email4")) {	
-				driver.findElement(By.id("email")).sendKeys(" ");
-				
-			}else {
-				String userEmail = (createUsername() + "@test.com");
-				driver.findElement(By.id("email")).sendKeys(userEmail);
+
+	@Given("I also add {string} to create my account")
+	public void addUserName(String userName) {
+		String manySigns = "";
+
+		if(userName.contains("onehundredusername")) {
+			
+			for(int i = 0; i < 100; i++) {
+				manySigns = manySigns.concat("b");
 			}
-		}
+			sendKeysById("new_username", createUsername() + manySigns);
+			
+		} else if (userName.contains("occupiedusername")) {
+			  sendKeysById("new_username", "20210404160900@test.com");
+			  
+		} else
+			sendKeysById("new_username", createUsername());
+	}
 
-		//Using same user-id as in the e-mail address 
-		@Given("I also add {string} to create my account")
-		public void i_also_add_to_create_my_account(String userName) {			
-			
-			if (userName.contains("userid1")) {
-					driver.findElement(By.id("new_username")).sendKeys(createUsername());
-					System.out.println("Skapar userid1, som är unikt");
-					
-			}else if (userName.contains("userid2")) {
-					driver.findElement(By.id("new_username")).sendKeys(createUsername() +
-					"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa123");
-			}else if (userName.contains("userid3")) {
-					driver.findElement(By.id("new_username")).sendKeys("20210404160900@test.com");
-			
-			}else if (userName.contains("userid4")){
-				System.out.println("Skapar userid4, ingen e-mail");
-				driver.findElement(By.id("new_username")).sendKeys(createUsername());
-			}
-		}
+	@Given("I enter password")
+	public void i_enter_password() {
 		
-		@Given("I enter password {string}")
-		public void i_enter_password(String userName) {
-			
-			String password = (createUsername() + "beA@");
-			driver.findElement(By.id("new_password")).sendKeys(password);
-		}
+		sendKeysById("new_password","Password1!");
+	}
 
-		@When("I click on the Sign-up button")
-		public void i_click_on_the_sign_up_button() {
-			
-			WebElement signUpButton = driver.findElement(By.id("create-account"));
-			signUpButton.click();
-		}
+
+	@When("I click on the Sign-up button")
+	public void i_click_on_the_sign_up_button() {
+		click(By.id("create-account"));
+	}
+
+	@Then("The account creation shold be {string}")
+	public void the_account_creation_shold_be(String verification) {
+		
+		
+			if (verification.equals("success")) {
+				element = driver.findElement(By.cssSelector("div[class=signup-wrap]"));
+				assertEquals("signup-wrap", element.getAttribute("class"));
 				
-		@Then("The account creation should be {string} for user {string}")
-		public void the_account_creation_should_be_for_user(String verification, String userName) {
-				driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-			
-			try {
-					if(userName.contains("userid1")) {
-						WebElement element = driver.findElement(By.id("login"));
-						assertEquals(verification, element.getAttribute("id"));
-						System.out.println("Your selected username is OK");
-						
-					}else if (userName.contains("userid2")) {
-						WebElement element = driver.findElement(By.cssSelector("class[invalid-error$='100 characters long']"));
-						assertEquals(verification, element.getAttribute("class"));
-						System.out.println("Too many characters, use less then 100 characters");
-						
-					}else if (userName.contains("userid3"))	{
-						WebElement element = driver.findElement(By.cssSelector("class[invalid-error$ = 'Spooky.']"));
-						assertEquals(verification, element.getAttribute("class"));
-						System.out.println("The username is already taken");
-					
-					}else {
-						
-						WebElement element = driver.findElement(By.cssSelector("class[invalid-error = 'Please enter a value']"));
-						assertEquals(verification, element.getAttribute("id"));
-						System.out.println("No e-mail");
-					}
-					
-				}catch (Exception e) {
-					System.out.println("You have type in wrong username or forgot e-mail");
-				}
+
+			} else if (verification.equals("toManyCharacters")) {
+				element = driver.findElement(By.cssSelector("span[class=invalid-error]"));
+				assertEquals("Enter a value less than 100 characters long", element.getText());
+				
+
+			} else if (verification.equals("occupied")) {
+				element = driver.findElement(By.cssSelector("span[class=invalid-error]"));
+				assertEquals("Another user with this username already exists. "
+						+ "Maybe it's your evil twin. Spooky.", element.getText());
+				
+			} else {
+				element = driver.findElement(By.cssSelector("span[class=invalid-error]"));
+				assertEquals("Please enter a value", element.getText());
+			}
+
 			driver.close();
-		}
-				
-		    
+	}
 		
 	
-		
-		private void click(WebDriver driver, By by) {
-			(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
-					driver.findElement(by).click();
-		}
 
+	private void click(By by) {
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.elementToBeClickable(by));
+		driver.findElement(by).click();
+	}
 
-		public static void sendKeys(WebDriver driver, By by, String keys) {
-			(new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfAllElementsLocatedBy(by));
-				driver.findElement(by).sendKeys(keys);				
-		}
-		
-			
-		private String createUsername() {
-			//Collect current time
-			NowRightDate = LocalDateTime.now();  
-		    //Format the date to desired format   
-		    formDate = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");  
-		    
-		    String date = NowRightDate.format(formDate);  
-		   		return date;
-		}
-		
-					
-		/*private WebDriver createBrowser(String browser) {
-			
-			WebDriver driver;
-			
-			if(browser.equals("chrome")) {
-			
-				System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Selenium\\chromedriver.exe");
-				driver = new ChromeDriver();
-				
-			}else if(browser.equals("firefox")) {
-		    
-				System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\Selenium\\geckodriver.exe");
-				driver = new FirefoxDriver();
-			}else {
-				System.setProperty("webdriver.edge.driver", "C:\\Program Files\\Selenium\\msedgedriver.exe");
-				driver = new EdgeDriver();			
-			}
-			
-		    return driver; 
-		}*/
-	
-	
+	private void sendKeysById(String id, String keys) {
+		(new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id(id)));
+		driver.findElement(By.id(id)).sendKeys(keys);
+	}
+
+	private String createUsername() {
+		// Collect current time
+		NowRightDate = LocalDateTime.now();
+		// Format the date to desired format
+		formDate = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+
+		String date = NowRightDate.format(formDate);
+		return date;
+	}
+
 }
-
-
-
-
-
-
-
-
-
--- src/test/resources/Features/registration.feature
+++ src/test/resources/Features/registration.feature
@@ -1,18 +1,22 @@
 Feature: Registration of a mail account
-  I want to create different scenarios to secure correct error handling when I create mail accounts 
-  which are not correct
+  I want to create different scenarios to secure correct error handling when I create different mail accounts 
+  which some are not correct
 
   Scenario Outline: Create a Mailchimp account
+  
+
   	
     Given I add my <email> to create an Mialchimp account
     And I also add <username> to create my account
-    And I enter password <password>
+    And I enter password 
     When I click on the Sign-up button
-    Then The account creation should be <status> for user <username>
+    Then The account creation shold be <status>
 
     Examples: 
-     	|email    	 | username       |password		 | status  					       |
-   	  | "email1"   | "userid1"      |"password1" |"login" 						     |
-      | "email2"   | "userid2"     	|"password2" |"100 characters long"    |
-      | "email3"   | "userid3"      |"password3" |"Spooky."						     |
-    	| "email4"   | "userid4"      |"password4" |"Please enter a value"   |
+     |email							 |username				    |status             |
+     |"myEmail@test.com" |"randomusername"    |"success"          |	
+     |"myEmail@test.com" |"onehundredusername"|"toManyCharacters" |
+     |"myEmail@test.com" |"occupiedusername"  |"occupied"         |
+     |" " 							  |"randomusername"   |"forgotEmail"      |
+
+    	
